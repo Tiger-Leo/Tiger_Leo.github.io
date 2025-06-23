999ef742cdba18dd3e68070eb5ae120e8d9c0ca5
Tiger-Leo <60700348+Tiger-Leo@users.noreply.github.com>
2025-06-23 10:46:16 +0800

    Update search-box.js

--- components/search-box.js
+++ components/search-box.js
@@ -1,6 +1,6 @@
 // SEARCH BOX - contains code (logic) for search box on the front page
 
-var ENGINE = "baidu"; // google or ddg
+var ENGINE = "bing"; // google or ddg
 var search_box = document.getElementsByClassName('search-box')[0];
 var search_parent = search_box.parentNode;
 
@@ -27,7 +27,7 @@ search_box.onkeypress = function(e) {
 // ** BANGS **
 // shortcuts to common websites
 var queries = {
-    "!baidu": "https://www.baidu.com/s?wd=",
+    "!bing": "https://cn.bing.com/?mkt=zh-CN",
     "!scholar": "https://scholar.google.com/scholar?q=",
     "!mail": "https://mail.qq.com/",
     "!youtube": "https://www.youtube.com/results?search_query=",
@@ -42,7 +42,7 @@ var queries = {
 
 
 var searchEngines = {
-    "baidu": "https://www.baidu.com/s?wd=",
+    "bing": "https://cn.bing.com/?mkt=zh-CN",
 };
 
 
